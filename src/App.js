fa43e2175751dd52e888c02a936964ffc37a879c
Ibahm <ibrahimahm92@gmail.com>
2022-08-14 18:19:53 +0100

    Added checkout calculations in CheckoutList component
    - Subtotal is calculated from all items in cart
    - Delivery fee is shown if conditions are met or it is free
    - Total is calculated by adding the delivery fee

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import ItemList from "./components/ItemList";
-import { CartProvider } from "./context/CartContext";
 
 const App = () => {
   return (
--- src/components/CheckoutList.js
+++ src/components/CheckoutList.js
@@ -1,15 +1,26 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import CheckoutItem from "./CheckoutItem";
 import { CartContext } from "../context/CartContext";
 
 const CheckoutList = () => {
     const {cart} = useContext(CartContext);
+    const prices = cart.map(item => item.price);
+    const subTotal = prices.reduce((pVal, cVal) => (pVal + cVal), 0);
 
     return (
-        <div className="checkout-list">
-            {cart.map(item => (
-                <CheckoutItem id={item.id} name={item.name} price={item.price}/>
-            ))}
+        <div className="checkout-cont">
+            <div className="checkout-list">
+                {cart.map(item => (
+                    <CheckoutItem id={item.id} name={item.name} price={item.price}/>
+                ))}
+            </div>
+            <div className="checkout-info">
+                <h2>Total</h2>
+                <h4>Sub-Total: £{(subTotal.toFixed(2))}</h4>
+                <h4>Delivery: {subTotal >= 35 ? "FREE" : "£4.99"}</h4>
+                <h3>Total: {subTotal >= 35 ? subTotal.toFixed(2) : (subTotal + 4.99).toFixed(2)}</h3>
+                <button className="checkout-button">CHECKOUT</button>
+            </div>
         </div>
     )
 }
